2ff91c08c35b623cbdd3899f9fbe9a705c006d26
gizmo0075 <89253368+gizmo0075@users.noreply.github.com>
2025-12-03 09:59:19 +0000

    Add files via upload
    from kodi-root

---
+++ zips/plugin.mybuild/default.py
@@ -0,0 +1,17 @@
+﻿import xbmcplugin, xbmcgui, xbmcaddon
+import sys
+
+addon = xbmcaddon.Addon()
+addon_name = addon.getAddonInfo('name')
+
+xbmcplugin.setContent(int(sys.argv[1]), 'videos')
+
+# Create a simple list item
+url = 'https://example.com/video.mp4'
+li = xbmcgui.ListItem(label='Test Video')
+li.setInfo('video', {'title': 'Test Video'})
+li.setProperty('IsPlayable', 'true')
+
+# Add item to directory
+xbmcplugin.addDirectoryItem(int(sys.argv[1]), url, li, isFolder=False)
+xbmcplugin.endOfDirectory(int(sys.argv[1]))
